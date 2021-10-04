ee0939c1995539a849e20277326baf2bc17cf780
Gyana Ranjan panda <83230804+lonewolf2002@users.noreply.github.com>
2021-10-04 22:48:25 +0530

    add my portfolio

---
+++ codes/cool_dark_portfolio/side nav/side.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
