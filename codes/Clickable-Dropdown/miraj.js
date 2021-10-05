116af793cd1e965a392bdd7493d085da241fa9e5
Miraj8280 <mirajasraf786@gmail.com>
2021-10-05 12:37:43 +0530

    added clickable dropdown menu

---
+++ codes/Clickable-Dropdown/miraj.js
@@ -0,0 +1,17 @@
+
+function Miraj() {
+    document.getElementById("miraj").classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("drop-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
