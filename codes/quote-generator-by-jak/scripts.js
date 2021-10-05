d7d6bb576556262977a9ec7175b7e8fb56d0e730
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-10-05 11:32:51 +0530

    Quote Generator by Jonak Adipta Kalita!!

---
+++ codes/quote-generator-by-jak/scripts.js
@@ -0,0 +1,25 @@
+const dex = document.getElementById("dex");
+
+const fetchData = async () => {
+	const url = "https://type.fit/api/quotes";
+	const res = await fetch(url);
+	const data = await res.json();
+	displayData(data);
+};
+
+const displayData = (data) => {
+	const quoteHTMLString = data.map((item) => {		
+		console.log(item);
+		
+		return (`
+			<li class="card">
+				<h2 class="card-title">${item.text}</h2>
+				<p>By: ${item.author}</p>
+			</li>
+		`);
+	}).join('');
+
+    dex.innerHTML = quoteHTMLString;
+};
+
+fetchData();
